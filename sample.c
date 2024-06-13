16a18f10274832978ffcbfd8f8ffaf63dcf25fb9
Rananjay-chauhan <rananjaychauhan28@gmail.com>
2024-06-13 10:39:14 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -5,6 +5,7 @@
 #define BUFSIZE 256
                                 
 // This program prints the size of a specified file in bytes
+
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
